6d3da35a30738fe77d71868c8dbd332f5b8aa1ab
Mateus <mateuseduardoteuta10@gmail.com>
2023-06-10 09:58:05 -0300

    validando entradas em branco

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,6 +1,15 @@
 const userData = require('../data/userData')
+const AppError = require('../utils/AppError')
+
+const validData = (data) => {
+    return data === undefined || data === null || data === ''
+}
 
 exports.userService = async (data) => {
+
+    if (validData(data.email)) return new AppError('Email is required')
+    if (validData(data.senha)) return new AppError('Senha is required') 
+
     const result = await userData.getUser(data)
 
     return result
