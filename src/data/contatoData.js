e998ddad90230169c8aa9373e72737319fb1e75a
Mateus <mateuseduardoteuta10@gmail.com>
2023-06-10 10:13:02 -0300

    busca de contatos no bd

--- src/data/contatoData.js
+++ src/data/contatoData.js
@@ -9,3 +9,23 @@ exports.getCategoria = async () => {
 
     return result[0]
 }
+
+exports.getContatos = async (data) => {
+    const result = await conn.query(`
+        select c.nome,
+        c.email,
+        c.telefone,
+        c.url_github,
+        c.url_linkedin,
+        ca.idcategoria,
+        ca.nome,
+        cc.contatos_idcontatos
+        from controle_contato_usuario cc
+        inner join contatos c on cc.contatos_idcontatos = c.idcontatos
+        inner join categoria ca on ca.idcategoria = c.categoria_idcategoria
+        inner join usuario u on cc.usuario_id_usuario = u.id_usuario
+        where cc.usuario_id_usuario = ?
+    `, [data.idUsuario])
+
+    return result[0]
+}
