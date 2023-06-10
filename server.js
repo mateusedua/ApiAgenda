8f7e7f75fc55edabc3a9d38c2693ff06161788af
Mateus <mateuseduardoteuta10@gmail.com>
2023-06-10 09:24:30 -0300

    iniciando o server com express

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const AppError = require('./src/utils/AppError')
+
+const app = express();
+
+const PORT = 3000
+
+app.use(express.json());
+
+app.use((error, req, res) => {
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({
+            statusCode: "error",
+            message: error.message
+        })
+    }
+
+    return res.status(500).json({
+        statusCode: "error",
+        message: "Internal Server Error"
+    })
+})
+
+app.listen(PORT, () => { `Server Start on port ${PORT}` });
